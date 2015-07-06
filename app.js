ac6c8621d8df7f0cc32ced75ab9430cc4f1c6dc9
AJ ONeal <awesome@coolaj86.com>
2015-07-06 11:13:24 -0600

    session with secret secret

--- app.js
+++ app.js
@@ -2,21 +2,31 @@
 
 module.exports = function () {
   var express = require('express');
+  var session = require('express-session');
   var bodyParser = require('body-parser');
   var app = express();
 
   app.use('/', bodyParser.urlencoded());
   app.use('/', bodyParser.json());
+  app.use('/', session({ secret: require('./config').cookieSecret }));
+
   app.use('/', function (req, res, next) {
+    if (!req.session.count) {
+      req.session.count = 0;
+    }
+    req.session.count += 1;
+
     console.log('hello');
     console.log(req.headers);
     console.log(req.body);
+    console.log(req.session);
     next();
   });
   app.use('/login-with-basic', function (req, res) {
     var user = require('basic-auth')(req);
     if (user && 'coolaj86' === user.name
         && 'supersecret' === user.pass) {
+      req.session.lastLogin = Date.now();
       res.send('Welcome!');
       return;
     }
@@ -32,10 +42,18 @@ module.exports = function () {
       return;
     }
 
-    res.setHeader('WWW-Authenticate', 'Basic realm=Authorization Required');
-    res.statusCode = 401;
     res.send('Fail!');
   });
+
+  app.use('/', function (req, res) {
+    // req.session.lastLoginType
+    if (req.session.lastLogin) {
+      res.send('Authenticated');
+      return;
+    }
+
+    res.send('Unauthenticated');
+  })
 
   return app;
 };
